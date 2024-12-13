4faf63a3307bdfdd116ff804e50fe7e6a09f106b
Lamin Jawla <laminjawla1001@gmail.com>
2024-12-13 21:51:04 +0000

    11. Largest and smallest

--- chapter_3/11_largest_and_smallest.py
+++ chapter_3/11_largest_and_smallest.py
@@ -4,3 +4,25 @@ Design a Python program with a loop that lets the user enter a series of numbers
 end of the series. After all the numbers have been entered, the program should display the largest and smallest numbers
 entered.
 """
+
+smallest = None
+largest = None
+
+number = int(input("Enter a number (-99 to end): "))
+
+while number != -99:
+    if smallest is None or largest is None:
+        smallest = number
+        largest = number
+    else:
+        if smallest > number:
+            smallest = number
+        if largest < number:
+            largest = number
+    number = int(input("Enter a number (-99 to end): "))
+
+if smallest is not None and largest is not None:
+    print("Smallest:", smallest)
+    print("Largest:", largest)
+else:
+    print("Can't determine smallest and largest.")
