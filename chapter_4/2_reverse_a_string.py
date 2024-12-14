c2805a32f78a3608c0f8fa036ae89ceac33c34ec
Lamin Jawla <laminjawla1001@gmail.com>
2024-12-14 11:47:32 +0000

    2. Reverse a string

---
+++ chapter_4/2_reverse_a_string.py
@@ -0,0 +1,8 @@
+"""
+2. Reverse a string
+Design a Python program that will ask the user to enter a string. The program will reverse and display the string.
+"""
+
+string = input("Enter a string: ")
+
+print("Reversed:", string[::-1])
