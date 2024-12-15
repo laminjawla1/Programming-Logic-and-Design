f0bf4cee05bd3eff9ee38dadad962565a4b1534e
Lamin Jawla <laminjawla1001@gmail.com>
2024-12-15 11:04:20 +0000

    Personal Information implemented in java

---
+++ exercises/Java/chapter_1/PersonalInformation.java
@@ -0,0 +1,15 @@
+// 1. Personal Information
+// Design a Python program that displays the following information:
+// • Your name
+// • Your address, village/town/city, region
+// • Your telephone number
+// • Your email address
+// • Your college major
+public class PersonalInformation {
+    public static void main(String[] args) {
+        System.out.println("Name: " + "Lamin Jawla");
+        System.out.println("Address: " + "Tintinbato, Busumbala");
+        System.out.println("Phone: " + "+220 2933536");
+        System.out.println("College Major: " + "Information Systems");
+    }
+}
